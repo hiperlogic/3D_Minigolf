a3c20676edb58285174fabfc8eb0c5afac69683b
Rodrigo (Spy) Domingues <rodrigod@hiperlogic.com.br>
2022-06-13 16:31:22 -0300

    Yeeee! Fly Ball, Fly! Now we can shoot!

---
+++ Items/Ball/Ball.gd
@@ -0,0 +1,23 @@
+extends RigidBody
+
+signal stopped
+
+func shoot(angle, power):
+	var force = Vector3(0,0,-2).rotated(Vector3(0,1,0), angle)
+	apply_impulse(Vector3(), force*power/5)
+	
+func _integrate_forces(state):
+	if state.linear_velocity.length()<0.1:
+		emit_signal("stopped")
+		state.linear_velocity = Vector3()
+		
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
